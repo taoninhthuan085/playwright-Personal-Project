a24dc849eaed5772d331911c17233d010eae98ef
taoninhthuan085 <taoninhthuan085@gmail.com>
2026-06-08 15:29:34 +0700

    Add code, run parallel Tcs

--- tests/LogIn/LogIn.spec.ts
+++ tests/LogIn/LogIn.spec.ts
@@ -1,6 +1,27 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../Pages/Log_in_Page';
 
+
+test.describe('Parallel', () => { 
+
+     test.describe.configure({ mode: 'parallel' });
+    
+    test('TC_LOGIN_001: Log in successfully', async({page}) => {
+    const loginpage = new LoginPage(page);
+    await loginpage.goto();
+    await loginpage.login('quynhtvn11+11@gmail.com','Thaytoo02');
+    await expect(page).toHaveURL('https://qc.atalink.com.vn/my-tasks/org_29ca83a5-a9f6-4241-b2c9-a14970ba2370/news');
+
+    })
+    test('TC_LOGIN_006: Log in with email and wrong password', async({page}) => {
+    const loginpage = new LoginPage(page);
+
+    await loginpage.goto();
+    await loginpage.login('quynhtvn11+11@gmail.com','Thaytoo0');
+    await expect(page.getByText('Email hoặc mật khẩu không đúng. Vui lòng thử lại.')).toBeVisible();
+
+    })
+});
 test('TC_LOGIN_001: Log in successfully', async({page}) => {
     const loginpage = new LoginPage(page);
 
