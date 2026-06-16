e619a7f3ff8ffccaaf5787f39b8cadfa7b2d9aee
taoninhthuan085 <taoninhthuan085@gmail.com>
2026-06-16 18:30:49 +0700

    Add file Regression test

---
+++ tests/RegressionTest/R_Login.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../Pages/Log_in_Page';
+
+test('TC_LOGIN_002: Log in successfully', async({page}) => {
+    const loginpage = new LoginPage(page);
+
+    await loginpage.goto();
+    await loginpage.login(process.env.ADMIN_USER!,process.env.ADMIN_PASS!);
+    await expect(page).toHaveURL(
+        /.*news$/,
+        { timeout: 30000 }
+    );
+
+})
+test('TC_LOGIN_004: Log in with email and wrong password', async({page}) => {
+    const loginpage = new LoginPage(page);
+
+    await loginpage.goto();
+    await loginpage.login('quynhtvn11@gmail.com','bbbbbbb');
+    await expect(page.getByText('Email hoặc mật khẩu không đúng. Vui lòng thử lại.')).toBeVisible();
+
+})
+
+test('TC_LOGIN_009: Log out successfully', async({page}) => {
+    const loginpage = new LoginPage(page);
+
+    await loginpage.goto();
+    await Promise.all([
+        page.waitForNavigation(),
+        await loginpage.login('quynhtvn11+11@gmail.com','Thaytoo02')
+    ]);
+    await page.goto('/my-profile');
+    await page.getByText('Đăng xuất').click();
+    await expect(page).toHaveURL('https://qc.atalink.com.vn/sign-in');
+})
