50a6ab8a4861f4dd21c2b07e4b43999f2079f08c
taoninhthuan085 <taoninhthuan085@gmail.com>
2026-06-19 15:51:05 +0700

    Add file Tcs Regression API

---
+++ tests/RegressionTest_API/R_ProcurementCategories_API.spec.ts
@@ -0,0 +1,85 @@
+import { test, expect } from '../../Fixtures/auth_fixture';
+import { OrgCategoryApi } from '../../api_src/org_P_CategoryAPI';
+import { OrgCategoryData } from '../../Data/org-category.data';
+
+test.describe('Org Category CRUD', () => {
+  test('Create -> Update -> Delete Category', async ({
+    request,
+    accessToken,
+  }) => {
+    const categoryApi = new OrgCategoryApi(request, accessToken);
+
+    let categoryId: string | undefined;
+
+    try {
+      // CREATE
+      //---------------------------------
+
+      const createPayload =
+        OrgCategoryData.createCategory();
+
+      const createResponse =
+        await categoryApi.createCategory(
+          createPayload
+        );
+ 
+      expect(createResponse.status()).toBe(201);
+
+      const createBody =
+        await createResponse.json();
+     console.log(createBody);
+
+      categoryId = createBody.data.id;
+      
+
+      expect(categoryId).toBeTruthy();
+
+    //   expect(
+    //     createBody.data.name.vi
+    //   ).toBe(createPayload.name.vi);
+
+      console.log(
+        `Created Category ID: ${categoryId}`
+      );
+
+      
+      // UPDATE
+      //---------------------------------
+
+      const updatePayload =
+        OrgCategoryData.updateCategory();
+
+      const updateResponse =
+        await categoryApi.updateCategory(
+          categoryId!,
+          updatePayload
+        );
+
+      expect(updateResponse.status()).toBe(200);
+
+      const updateBody =
+        await updateResponse.json();
+
+      console.log(
+        `Updated Category: ${updatePayload.name.vi}`
+      );
+    } finally {
+      
+      // CLEANUP
+      //---------------------------------
+
+      if (categoryId) {
+        const deleteResponse =
+          await categoryApi.deleteCategory(
+            categoryId
+          );
+
+        expect(deleteResponse.status()).toBe(200);
+
+        console.log(
+          `Deleted Category ID: ${categoryId}`
+        );
+      }
+    }
+  });
+});
