efb45e0f14893ffc27874a5b33026fd1a0f1d913
taoninhthuan085 <taoninhthuan085@gmail.com>
2026-06-19 15:10:18 +0700

    Add Tcs Regression test

--- tests/API_ProcurementCategory/delete_PCa_tcs.spec.ts
+++ tests/API_ProcurementCategory/delete_PCa_tcs.spec.ts
@@ -1,16 +1,10 @@
 import { test, expect } from '../../Fixtures/auth_fixture';
 import { OrgCategoryApi } from '../../api_src/org_P_CategoryAPI';
-import { OrgCategoryData } from '../../Data/org-category.data';
 
 test.describe('Delete Org Category API', () => {
-  const baseURL = 'https://qc-api.atalink.com.vn';
 
-  const orgId = 'cat_1780997000557_2cd6bc66-5bbd-4524-ab16-c020dd5a89df';
+  const categoryId ='cat_1780980589553_6ac39324-c68c-43eb-8197-42b45ab227c9';
 
-  const categoryId =
-    'cat_1780980589553_6ac39324-c68c-43eb-8197-42b45ab227c9';
-
-  const token = process.env.ACCESS_TOKEN!;
 
   test('Delete org category successfully', async ({request, accessToken}) => {
     
